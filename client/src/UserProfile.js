0607b7cc0349afeb36e3b24206d30fa48710a0e2
QiQing-Luo <42788279+QiQing-Luo@users.noreply.github.com>
2021-04-29 17:42:27 -1000

    Update UserProfile.js

--- client/src/UserProfile.js
+++ client/src/UserProfile.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Header, Table, Input, Form, TextArea } from 'semantic-ui-react';
+import { Container, Header, Table, Input, Form, TextArea, Button } from 'semantic-ui-react';
 
 const UserProfile = () => {
     const [activeTab, setActiveTab] = useState('User Profile');
