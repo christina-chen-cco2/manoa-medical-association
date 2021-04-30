46a8990e63d0a8be7c1ef7432f618431ac829744
QiQing-Luo <42788279+QiQing-Luo@users.noreply.github.com>
2021-04-29 17:48:02 -1000

    Update UserProfile.js

--- client/src/UserProfile.js
+++ client/src/UserProfile.js
@@ -2,39 +2,6 @@ import React, { useState } from 'react';
 import { Container, Header, Table, Input, Form, TextArea, Button } from 'semantic-ui-react';
 
 const UserProfile = () => {
-    const [activeTab, setActiveTab] = useState('User Profile');
-    const [activePane, setActivePane] = useState(UserProfile);
-
-    const [ssn, setSSN] = useState('123-45-6789');
-    const [dob, setDOB] = useState('01/01/2021');
-    const [address, setAddress] = useState('1234 Abcd Street Apt 809');
-    const [insuranceId, setInsuranceId] = useState('123456789');
-    const [email, setEmail] = useState('abcxyz123789@gmail.com');
-    const [profileEditButton, setProfileEditButton] = useState('Edit');
-
-    const [tempSSN, setTempSSN] = useState(ssn);
-    const [tempDOB, setTempDOB] = useState(dob);
-    const [tempAddress, setTempAddress] = useState(address);
-    const [tempInsuranceId, setTempInsuranceId] = useState(insuranceId);
-    const [tempEmail, setTempEmail] = useState(email);
-
-    const setProfileEdit = () => {
-        if (profileEditButton === 'Edit') {
-            setProfileEditButton('Save');
-            setSSN(<Input fluid size='small' defaultValue={tempSSN} onChange={(event, data) => setTempSSN(data.value)}/>);
-            setDOB(<Input fluid size='small' defaultValue={tempDOB} onChange={(event, data) => setTempDOB(data.value)}/>);
-            setAddress(<Input fluid size='small' defaultValue={tempAddress} onChange={(event, data) => setTempAddress(data.value)}/>);
-            setInsuranceId(<Input fluid size='small' defaultValue={tempInsuranceId} onChange={(event, data) => setTempInsuranceId(data.value)}/>);
-            setEmail(<Input fluid size='small' defaultValue={tempEmail} onChange={(event, data) => setTempEmail(data.value)}/>);
-        } else {
-            setProfileEditButton('Edit');
-            setSSN(tempSSN);
-            setDOB(tempDOB);
-            setAddress(tempAddress);
-            setInsuranceId(tempInsuranceId);
-            setEmail(tempEmail);
-        }
-    }
 
     return (
         <Container>
@@ -89,7 +56,6 @@ const UserProfile = () => {
                     </Table.Cell>
                 </Table.Row>
             </Table>
-            <Button onClick={() => setProfileEdit()}>{profileEditButton}</Button>
         </Container>
     );
 }
