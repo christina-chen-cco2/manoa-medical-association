f6442b0f9faf141259fc7676dd20261a43d9010f
haochen-wang <54867326+haochen-wang@users.noreply.github.com>
2021-05-04 15:11:57 -1000

    updated buttons

--- client/src/Landing.js
+++ client/src/Landing.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react';
-import { Grid, Image, Button, Icon, Modal } from 'semantic-ui-react';
+import { Grid, Image, Button, Icon, Modal, Card } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
 class Landing extends Component {
@@ -7,12 +7,13 @@ class Landing extends Component {
   render() {
 
     return (
-        <div className='background'>
         <Grid container rows='4'>
           <Grid.Row centered>
             <Image size='big' centered src='/images/logo.png' />
           </Grid.Row>
           <Grid.Row centered>
+            <div className='button'>
+              <Card>
             <Button color='green' animated='fade' size='big' as={Link} to='/login'>
               <Button.Content visible>
                 <Icon name='sign in' inverted size='large'/>
@@ -20,6 +21,12 @@ class Landing extends Component {
               </Button.Content>
               <Button.Content hidden >CLICK TO SIGN IN</Button.Content>
             </Button>
+              </Card>
+            </div>
+          </Grid.Row>
+          <Grid.Row centered>
+            <div className='button'>
+              <Card>
             <Button color='blue' animated='fade' size='big' as={Link} to='/patient-signup'>
               <Button.Content visible>
                 <Icon name='add user' color='white' size='large'/>
@@ -27,8 +34,12 @@ class Landing extends Component {
               </Button.Content>
               <Button.Content hidden>CLICK TO SIGN UP</Button.Content>
             </Button>
+              </Card>
+            </div>
           </Grid.Row>
           <Grid.Row centered>
+            <div className='button'>
+              <Card>
             <Modal trigger={
               <Button color='purple' animated='fade' size='big'>
                 <Button.Content visible>
@@ -43,9 +54,10 @@ class Landing extends Component {
                 </Modal.Description>
               </Modal.Content>
             </Modal>
+            </Card>
+            </div>
           </Grid.Row>
         </Grid>
-        </div>
     );
   }
 }
